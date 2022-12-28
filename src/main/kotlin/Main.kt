6c17411b7ab9666c8793c9dce42fe04245136a3a
Maru Khan <romirov@gmail.com>
2022-12-29 01:01:39 +0300

    created fist proj and test for Otus

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package ru.otus.otuskotlin.m1l1
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
---
+++ src/test/kotlin/FistTest.kt
@@ -0,0 +1,11 @@
+package ru.otus.otuskotlin.m1l1
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class FistTest {
+    @Test
+    fun firstTest() {
+        assertEquals(3, 1 + 2)
+    }
+}
